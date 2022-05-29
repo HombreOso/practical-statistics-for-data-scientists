241e404e44979f268378a31957e50f77c72d71bb
ArthurEmig <arthur.erich.emig@gmail.com>
2022-05-30 00:42:25 +0200

    the folder _data_ was unappropriately named, it collided with one of numpy package files, renaming and correction in the common.py have been conducted

--- python/notebooks/common.py
+++ python/notebooks/common.py
@@ -9,9 +9,10 @@ def dataDirectory(dataDirectoryName='data'):
     If your setup is different, you will need to change this method.
     """
     dataDir = Path(__file__).resolve().parent
-    while not list(dataDir.rglob('data')):
+    print(f"__file__: {__file__}")
+    while not list(dataDir.rglob('data_book_gedeck')):
         dataDir = dataDir.parent
-    found = [d for d in dataDir.rglob('data') if d.is_dir()]
+    found = [d for d in dataDir.rglob('data_book_gedeck') if d.is_dir()]
     if not found:
         raise Exception(f'Cannot find data directory with name {dataDirectoryName} along the path of your source files')
     return found[0]
